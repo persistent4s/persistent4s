b125a69137a406f15214bd554c86168639a3a50a
Antonio Jimenez <antonio.jimenez.nieto@gmail.com>
2026-04-10 11:21:21 +0200

    Move routes to a different file

--- modules/examples/src/main/scala/persistent4s/examples/school/infrastructure/SchoolServer.scala
+++ modules/examples/src/main/scala/persistent4s/examples/school/infrastructure/SchoolServer.scala
@@ -16,49 +16,20 @@
 
 package persistent4s.examples.school.infrastructure
 
-import cats.effect.{IO, IOApp, Resource}
-import cats.syntax.all.*
+import cats.effect.{IO, IOApp}
 import com.comcast.ip4s.*
 import org.http4s.ember.server.EmberServerBuilder
-import org.http4s.HttpRoutes
-import smithy4s.http4s.SimpleRestJsonBuilder
-import smithy4s.http4s.swagger.docs
-
-import persistent4s.examples.school.api.*
-import persistent4s.examples.school.application.*
 
 object SchoolServer extends IOApp.Simple:
 
   def run: IO[Unit] =
     SchoolModule.make.use { module =>
-      val routes: Resource[IO, HttpRoutes[IO]] =
-        for
-          studentRoutes <- SimpleRestJsonBuilder
-                             .routes(StudentServiceImpl(module))
-                             .resource
-          courseRoutes <- SimpleRestJsonBuilder
-                            .routes(CourseServiceImpl(module))
-                            .resource
-          enrollmentRoutes <- SimpleRestJsonBuilder
-                                .routes(EnrollmentServiceImpl(module))
-                                .resource
-          eventsRoutes <- SimpleRestJsonBuilder
-                            .routes(EventsServiceImpl(module))
-                            .resource
-          docsRoutes = docs[IO](
-                         StudentService,
-                         CourseService,
-                         EnrollmentService,
-                         EventsService,
-                       )
-        yield studentRoutes <+> courseRoutes <+> enrollmentRoutes <+> eventsRoutes <+> docsRoutes
-
-      routes.use { r =>
+      SchoolRoutes.make(module).use { routes =>
         EmberServerBuilder
           .default[IO]
           .withHost(host"0.0.0.0")
           .withPort(port"8181")
-          .withHttpApp(r.orNotFound)
+          .withHttpApp(routes.orNotFound)
           .build
           .useForever
       }
