e6d057384b1c4b10bd50a161b0a2d49747ba266b
bjolidon <bastien.jolidon@epfl.ch>
2026-05-11 09:51:35 +0200

    Discard return balue in example

--- modules/examples/src/main/scala/persistent4s/examples/library/application/BorrowingService.scala
+++ modules/examples/src/main/scala/persistent4s/examples/library/application/BorrowingService.scala
@@ -36,6 +36,7 @@ class BorrowingServiceImpl(repository: BorrowingRepository[IO])(using EventStore
         case e if e.getMessage.contains("not found") => NotFoundError(e.getMessage)
         case e                                       => ValidationError(e.getMessage)
       }
+      .void
 
   def returnBook(bookId: String, memberId: String): IO[Unit] =
     ReturnBookHandler
@@ -44,6 +45,7 @@ class BorrowingServiceImpl(repository: BorrowingRepository[IO])(using EventStore
         case e if e.getMessage.contains("not found") => NotFoundError(e.getMessage)
         case e                                       => ValidationError(e.getMessage)
       }
+      .void
 
   def getBorrowings(): IO[GetBorrowingsOutput] =
     repository.getBorrowings.map(borrowings => GetBorrowingsOutput(borrowings.map(toBorrowingItem)))
