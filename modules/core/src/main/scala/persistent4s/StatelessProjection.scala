cdf653c1932c4da70575b03718722f7966a84fce
Antonio Jimenez <antonio.jimenez.nieto@gmail.com>
2026-06-12 13:21:11 +0200

    Update file header

--- modules/core/src/main/scala/persistent4s/StatelessProjection.scala
+++ modules/core/src/main/scala/persistent4s/StatelessProjection.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2026 Antonio Jimenez and Bastien Jolidon
+ * Copyright 2026 Bastien Jolidon
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
